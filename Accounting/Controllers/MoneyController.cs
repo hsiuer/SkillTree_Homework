d92fcd1f3a53c1627026740124de0b4549e7b325
cindywangtw <cindywangtw@hotmail.com>
2019-07-09 14:54:16 +0800

    Create Accounting

---
+++ Accounting/Controllers/MoneyController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Accounting.Models.ViewModels;
+
+namespace Accounting.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Index()
+        {
+            MoneyViewModel moneyViewModel = new MoneyViewModel
+            {
+                Types = GetTypes()
+            };
+
+            return View(moneyViewModel);
+        }
+
+        protected List<SelectListItem> GetTypes()
+        {
+            List<SelectListItem> ddlTypes = new List<SelectListItem>();
+            ddlTypes.Add(new SelectListItem() { Text = "請選擇", Value = "", Selected = true });
+            ddlTypes.Add(new SelectListItem() { Text = "支出", Value = "O", Selected = false });
+            ddlTypes.Add(new SelectListItem() { Text = "收入", Value = "I", Selected = false });
+
+            return ddlTypes;
+        }
+
+        [ChildActionOnly]
+        public ActionResult ShowHistory()
+        {
+            List<MoneyViewModel> history = new List<MoneyViewModel>();
+            string[] str = { "支出", "收入" };
+            DateTime start = new DateTime(2006, 1, 1);
+            int range = (DateTime.Today - start).Days;
+
+            var random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var Category = str[random.Next(str.Length)];
+                var Money = random.Next(100, 20000);
+                var Day=start.AddDays(random.Next(range));
+
+                history.Add(new MoneyViewModel(Category, Money, Day/*, string.Empty*/));
+            }
+            return View(history);
+        }
+
+    }
+}
---
+++ Accounting/Models/ViewModels/MoneyViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Accounting.Models.ViewModels
+{
+    public class MoneyViewModel
+    {
+        public MoneyViewModel()
+        {
+            //建構子
+        }
+
+        public MoneyViewModel(string types, int money, DateTime date/*, string note*/)
+        {
+            this.SelectedTypes = types;
+            this.Money = money;
+            this.Date = date;
+            //this.Note = note;
+        }
+
+        public string SelectedTypes { get; set; }
+
+        [Required]
+        [Display(Name = "類別")]
+        public IEnumerable<SelectListItem> Types { get; set; }
+
+        [Required]
+        [Display(Name = "金額")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int Money { get; set; }
+
+        [Required]
+        [Display(Name = "日期")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "備註")]
+        public string Note { get; set; }
+    }
+}
