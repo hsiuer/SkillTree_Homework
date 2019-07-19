6528770cdb62810aa660023fe1f696938a2dd974
cindywangtw <cindywangtw@hotmail.com>
2019-07-19 10:45:37 +0800

    將記帳本呈現資料的部分改為真 DB

---
+++ Accounting/Models/Data/DataAccess.cs
@@ -0,0 +1,28 @@
+﻿using Accounting.Models.EntityFramework;
+using Accounting.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Accounting.Models.Data
+{
+    public class DataAccess : IDataAccess
+    {
+
+        public IEnumerable<MoneyViewModel> GetList()
+        {
+            using (var _accountData = new SkillTreeHomeworkEntities())
+            {
+                return (from c in _accountData.AccountBook
+                        select new MoneyViewModel
+                        {
+                            SelectedTypes = c.Categoryyy.ToString(),
+                            Date = c.Dateee,
+                            Money = c.Amounttt,
+                            Note = c.Remarkkk
+                        }).ToList();
+            }
+        }
+    }
+}
---
+++ Accounting/Models/Data/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Accounting.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Models.Data
+{
+    interface IDataAccess
+    {
+        IEnumerable<MoneyViewModel> GetList();
+    }
+}
