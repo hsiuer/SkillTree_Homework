91ac5b66656f4c26c8d65befab39550e3f39976f
cindywangtw <cindywangtw@hotmail.com>
2019-07-19 10:43:14 +0800

    將記帳本呈現資料的部分改為真 DB

--- Accounting/Controllers/MoneyController.cs
+++ Accounting/Controllers/MoneyController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Accounting.Models.Data;
+using Accounting.Models.EntityFramework;
 using Accounting.Models.ViewModels;
 
 namespace Accounting.Controllers
 {
     public class MoneyController : Controller
     {
+        DataAccess _dataAccess = new DataAccess();
+
         // GET: Money
         public ActionResult Index()
         {
@@ -18,14 +22,14 @@ namespace Accounting.Controllers
             };
 
             return View(moneyViewModel);
-        }
+        }     
 
         protected List<SelectListItem> GetTypes()
         {
             List<SelectListItem> ddlTypes = new List<SelectListItem>();
             ddlTypes.Add(new SelectListItem() { Text = "請選擇", Value = "", Selected = true });
-            ddlTypes.Add(new SelectListItem() { Text = "支出", Value = "O", Selected = false });
-            ddlTypes.Add(new SelectListItem() { Text = "收入", Value = "I", Selected = false });
+            ddlTypes.Add(new SelectListItem() { Text = "支出", Value = "0", Selected = false });
+            ddlTypes.Add(new SelectListItem() { Text = "收入", Value = "1", Selected = false });
 
             return ddlTypes;
         }
@@ -33,21 +37,23 @@ namespace Accounting.Controllers
         [ChildActionOnly]
         public ActionResult ShowHistory()
         {
-            List<MoneyViewModel> history = new List<MoneyViewModel>();
-            string[] str = { "支出", "收入" };
-            DateTime start = new DateTime(2006, 1, 1);
-            int range = (DateTime.Today - start).Days;
+            //List<MoneyViewModel> history = new List<MoneyViewModel>();
+            //string[] str = { "支出", "收入" };
+            //DateTime start = new DateTime(2006, 1, 1);
+            //int range = (DateTime.Today - start).Days;
 
-            var random = new Random();
-            for (int i = 0; i < 100; i++)
-            {
-                var Category = str[random.Next(str.Length)];
-                var Money = random.Next(100, 20000);
-                var Day=start.AddDays(random.Next(range));
+            //var random = new Random();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    var Category = str[random.Next(str.Length)];
+            //    var Money = random.Next(100, 20000);
+            //    var Day=start.AddDays(random.Next(range));
 
-                history.Add(new MoneyViewModel(Category, Money, Day/*, string.Empty*/));
-            }
-            return View(history);
+            //    history.Add(new MoneyViewModel(Category, Money, Day/*, string.Empty*/));
+            //}
+            //return View(history);
+            var data = _dataAccess.GetList();
+            return View(data);
         }
 
     }
