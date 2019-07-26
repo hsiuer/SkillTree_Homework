7b228e81ced2970126e468f9c6e4776842969ff1
cindywangtw <cindywangtw@hotmail.com>
2019-07-26 11:31:48 +0800

    欄位必填

---
+++ Accounting/Helpers/CategoryHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Accounting.Helpers
+{
+    public static class CategoryHelper
+    {
+        public static HtmlString DisplayCategoryColor(this HtmlHelper htmlHelper ,int types)
+        {
+            var className = "";
+            var CategoryName = "";
+
+            if (types==0)
+            {
+                className = "primary";
+                CategoryName = "收入";
+
+            }
+            else
+            {
+                className = "danger";
+                CategoryName = "支出";
+            }
+            return new MvcHtmlString($"<span class='text-{className}'>{CategoryName}</span>");
+        }
+    }
+}
